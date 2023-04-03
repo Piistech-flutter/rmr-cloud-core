478ae05aaf41d641088f500098cf5f39b8a7b60b
Piistech-flutter <omar.piistech@gmail.com>
2023-04-03 11:32:18 +0600

    init

---
+++ bin/rmr_cloud_core.dart
@@ -0,0 +1,5 @@
+import 'package:rmr_cloud_core/rmr_cloud_core.dart' as rmr_cloud_core;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rmr_cloud_core.calculate()}!');
+}
---
+++ test/rmr_cloud_core_test.dart
@@ -0,0 +1,8 @@
+import 'package:rmr_cloud_core/rmr_cloud_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
