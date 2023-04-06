c4da7c1318c01b48c57f22c316536e7b2489546b
Piistech-flutter <omar.piistech@gmail.com>
2023-04-06 10:52:56 +0600

    enum exported

--- bin/rmr_cloud_core.dart
+++
@@ -1,5 +0,0 @@
-import 'package:rmr_cloud_core/rmr_cloud_core.dart' as rmr_cloud_core;
-
-void main(List<String> arguments) {
-  print('Hello world: ${rmr_cloud_core.calculate()}!');
-}
--- test/rmr_cloud_core_test.dart
+++ test/rmr_cloud_core_test.dart
@@ -1,8 +1,1 @@
-import 'package:rmr_cloud_core/rmr_cloud_core.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
